a046c303a56d37d768e063ca352401e7b81674e8
Pavel Bulochkin <pavel.bulochkin@banuba.com>
2018-04-15 16:25:04 +0300

    Homework week 8. Finding closest centroids. Computing centroid means.

--- ML Stanford/homeworks/machine-learning-ex7/ex7/findClosestCentroids.m
+++ ML Stanford/homeworks/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,19 +15,33 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+for i = 1 : size(X, 1)
+  index_min = 1;
+  distance_min = norm(X(i, :) - centroids(1, :));
+  for j = 2 : K
+    distance_current = norm(X(i, :) - centroids(j, :));
+    if(distance_current < distance_min)
+      index_min = j;
+      distance_min = distance_current;
+    end
+  end
+  idx(i) = index_min;
+end
 
+% for i = 1 : size(X,1)
+%   printf("%d - %d \n", i, X(i, :))
+% end
 
-
+% X
 
 
 
 % =============================================================
 
 end
-
